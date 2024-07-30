bf9489c9a96e819771b1a6228838c48defff447e
grizeus <zaytsewww@gmail.com>
2024-07-30 03:00:10 +0300

    add base logic with timeouts

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,56 +3,56 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const inputDelay = document.querySelector("[name=delay]");
-const inputState = document.querySelector("[name=state]");
-const buttonSubmit = document.querySelector("button[type=submit]");
+const form = document.querySelector(".form");
 
-buttonSubmit.addEventListener("click", (event) => {
-  event.preventDefault();
+form.addEventListener("submit", e => {
+  e.preventDefault();
 
+  const inputDelay = document.querySelector("[name=delay]");
+  const inputState = document.querySelector("[name=state]:checked");
   const delay = parseInt(inputDelay.value, 10);
-  const state = inputState.value;
 
     if (delay > 0) {
-      console.log(state);
-      if (state === "fulfilled") {
-          const fulfilledToast = setTimeout(() => {
-              iziToast.success({
-                  title: "Notification",
-                  titleColor: "#fff",
-                  titleSize: "16px",
-                  titleLineHeight: 1.5,
-                  message: "Notification sent",
-                  messageSize: "16px",
-                  messageLineHeight: 1.5,
-                  messageColor: "#fff",
-                  backgroundColor: "#00b894",
-                  position: "topRight",
-                  closeOnEscape: true,
-                  icon: "success",
-                  timeout: 3000,
-                  theme: "dark",
-              });
-          }, delay);
-      } else if (state === "rejected") {
-          const rejectedToast = setTimeout(() => {
-              iziToast.error({
-                  title: "Notification",
-                  titleColor: "#fff",
-                  titleSize: "16px",
-                  titleLineHeight: 1.5,
-                  message: "Notification sent",
-                  messageSize: "16px",
-                  messageLineHeight: 1.5,
-                  messageColor: "#fff",
-                  backgroundColor: "#ef4040",
-                  position: "topRight",
-                  closeOnEscape: true,
-                  icon: "error",
-                  timeout: 3000,
-                  theme: "dark",
-              });
-          }, delay);
-      }
+    if (inputState.value === "fulfilled") {
+      const fulfilledToast = setTimeout(() => {
+        iziToast.success({
+          title: "Notification",
+          titleColor: "#fff",
+          titleSize: "16px",
+          titleLineHeight: 1.5,
+          message: "Notification sent",
+          messageSize: "16px",
+          messageLineHeight: 1.5,
+          messageColor: "#fff",
+          backgroundColor: "#00b894",
+          position: "topRight",
+          closeOnEscape: true,
+          icon: "success",
+          timeout: 3000,
+          theme: "dark",
+        });
+      }, delay);
+    } else if (inputState.value === "rejected") {
+      const rejectedToast = setTimeout(() => {
+        iziToast.error({
+          title: "Notification",
+          titleColor: "#fff",
+          titleSize: "16px",
+          titleLineHeight: 1.5,
+          message: "Notification sent",
+          messageSize: "16px",
+          messageLineHeight: 1.5,
+          messageColor: "#fff",
+          backgroundColor: "#ef4040",
+          position: "topRight",
+          closeOnEscape: true,
+          icon: "error",
+          timeout: 3000,
+          theme: "dark",
+        });
+      }, delay);
+    }
+  } else {
+    console.log("Delay must be greater than 0");
   }
 });
